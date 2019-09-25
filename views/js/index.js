f6411d8a994ea8f4397158e7e02e3a99490c31c2
Mousemices <9.1271613xia@gmail.com>
2019-09-25 14:25:58 +0200

    hamburger menu responsive only <800px

---
+++ views/js/index.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $('.nav-icon').click(function(){
+        $(this).toggleClass('active');
+        $('.nav-list').toggleClass('switch');  
+    });
+});
+
+//var categoria= document.getElementById("bathroom");
+
+/* categoria.addEventListener('mouseenter', e =>{
+    var element = document.getElementById("dark");
+    var element2 = document.getElementById("toto");
+    element.style.display="none";
+    element2.style.display="none";
+});
+
+categoria.addEventListener('mouseenter', e =>{
+    var element = document.getElementById("dark");
+    var element2 = document.getElementById("toto");
+    element.style.display="none";
+    element2.style.display="none";
+}); */
+
+/* function hideTitleAndDark() {
+    var element = document.getElementById("dark");
+    var element2 = document.getElementById("toto");
+    element.style.display="none";
+    element2.style.display="none";
+}
+  function showTitleAndDark() {
+    var element = document.getElementById("dark");
+    var element2 = document.getElementById("toto");
+    element.style.display="block";
+    element2.style.display="block";
+  } 
+
+
+ */
+
+/* function mouseOut(){
+    document.getElementById("aaaaa").style.display="flex";
+} */
+/* bathroom.addEventListener('mouseenter',function(){
+    bathroom.classList.remove('dark');
+});
+bathroom.addEventListener('mouseleave',function(){
+    bathroom.classList.add('dark');
+});
+ */
+
+/*    work--> Remove darkness and title to be display:none;
+
+ function myFunction() {
+    var element = document.getElementById("dark");
+    var element2 = document.getElementById("toto");
+    element.style.display="none";
+    element2.style.display="none";
+  } */
